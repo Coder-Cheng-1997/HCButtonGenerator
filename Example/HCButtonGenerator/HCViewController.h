9c229cd9c8f7153723ed75f259887557f8ed2596
CodeComposer <59467110+CodeComposer01@users.noreply.github.com>
2022-04-29 08:48:58 +0800

    Add files via upload

---
+++ Example/HCButtonGenerator/HCViewController.h
@@ -0,0 +1,13 @@
+//
+//  HCViewController.h
+//  HCButtonGenerator
+//
+//  Created by houcheng on 04/27/2022.
+//  Copyright (c) 2022 houcheng. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HCViewController : UIViewController
+
+@end
---
+++ Example/Pods/Target Support Files/HCButtonGenerator/HCButtonGenerator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HCButtonGenerator : NSObject
+@end
+@implementation PodsDummy_HCButtonGenerator
+@end
